4fd50c1a4ad67974887f66fc1dd16453d6f5d442
nuagenic <nuagenic@gmail.com>
2024-09-04 10:01:42 +0900

    feat: 헤더 라우팅 작업

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,25 @@
+import Link from "next/link";
+
 export default function Header() {
-  return <header>hello</header>;
+  const tabs = [
+    { name: "HOME", path: "/" },
+    { name: "RULES & TERMS", path: "/rules-terms" },
+    { name: "ARCHIVE", path: "/archive" },
+    { name: "ABOUT", path: "/about" },
+    { name: "SUBSCRIBE", path: "/subscribe" },
+  ];
+
+  return (
+    <header>
+      <nav className="w-full flex justify-between p-4">
+        {tabs.map((tab, i) => {
+          return (
+            <Link href={tab.path} key={i}>
+              <button key={i}>{tab.name}</button>
+            </Link>
+          );
+        })}
+      </nav>
+    </header>
+  );
 }
