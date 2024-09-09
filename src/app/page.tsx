380f904bfe0469d3da9182871a0b25c0287ae638
nuagenic <nuagenic@gmail.com>
2024-09-09 20:08:45 +0900

    feat: Connect DB to main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,19 @@
  */
 
 import Card from "@/components/Card";
+import { fetchCards } from "@/lib/fetchCards";
 
-export default function Home() {
+export default async function Home() {
+  const fetchedCards = await fetchCards();
+  const cards = fetchedCards.map((card) => {
+    const { _id, ...rest } = card;
+    return { id: _id.toString(), ...rest };
+  });
+  console.log(cards);
+
   return (
     <main className="overflow-hidden w-full h-full">
-      <Card />
+      <Card cards={cards} />
     </main>
   );
 }
--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -2,14 +2,19 @@
 
 import { useState } from "react";
 
-export default function Card() {
+export default function Card({ cards }) {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [currentIndex2, setCurrentIndex2] = useState(1);
   const [rotationAngle, setRotationAngle] = useState(0);
   const handleFlip = (event: React.MouseEvent<HTMLDivElement>) => {
     const cardWidth = event.currentTarget.offsetWidth;
     event.clientX > cardWidth / 2
       ? setRotationAngle((prevAngle) => prevAngle + 180)
       : setRotationAngle((prevAngle) => prevAngle - 180);
+    setCurrentIndex((prevIndex) => prevIndex + 2);
+    setCurrentIndex2((prevIndex) => prevIndex + 2);
   };
+
   return (
     <div className="w-full h-full [perspective:1000px]" onClick={handleFlip}>
       <div className="flex justify-center items-center w-full h-full transform transition-transform duration-700 ease-in-out [perspective:1000px]">
@@ -17,13 +22,13 @@ export default function Card() {
           className="absolute w-1/2vh h-1/2vh bg-red-100 text-white flex items-center justify-center [backface-visibility:hidden] transform transition-transform duration-700 ease-in-out"
           style={{ transform: `rotateY(${rotationAngle}deg)` }}
         >
-          Front Side
+          <img src={cards[currentIndex].content} />
         </div>
         <div
           className="absolute w-1/2vh h-1/2vh bg-blue-100 text-white flex items-center justify-center [backface-visibility:hidden] transform transition-transform duration-700 ease-in-out"
           style={{ transform: `rotateY(${rotationAngle + 180}deg)` }}
         >
-          Back Side
+          <div>{cards[currentIndex2].content}</div>
         </div>
       </div>
     </div>
