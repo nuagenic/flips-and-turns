8569c93a06d90e7f205bdcfcdc8b2dde3dc74324
nuagenic <nuagenic@gmail.com>
2024-10-04 00:20:42 +0900

    feat: Complete Caption Component

--- src/components/Caption/index.tsx
+++ src/components/Caption/index.tsx
@@ -3,10 +3,35 @@
 import { CardType } from "@/app/page";
 
 type Props = {
-  card: CardType;
+  cards: CardType[];
   currentIndex: number;
 };
 
-export default function Caption({ card, currentIndex }: Props) {
-  return;
+export default function Caption({ cards, currentIndex }: Props) {
+  const currentCard = cards[currentIndex];
+  const date = currentCard.createdAt;
+  function parseDate(num: number) {
+    return num < 10 ? "0" + num.toString() : num.toString();
+  }
+  const formattedDate =
+    date.getFullYear().toString().slice(2) +
+    "." +
+    parseDate(date.getMonth() + 1) +
+    "." +
+    parseDate(date.getDate());
+
+  const caption =
+    currentCard.createdBy +
+    "  |  " +
+    formattedDate +
+    "  |  " +
+    (currentIndex + 1) +
+    " / " +
+    cards.length;
+
+  return (
+    <div className="flex justify-center mt-2 mb-10 text-sm font-medium whitespace-pre">
+      {caption}
+    </div>
+  );
 }
