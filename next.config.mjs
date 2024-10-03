5f7105b85702dffb7596db01e14776444cda7a3e
nuagenic <nuagenic@gmail.com>
2024-10-03 16:28:49 +0900

    feat: Modify next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "max-age=0, s-maxage=0",
+          },
+        ],
+      },
+    ];
+  },
+};
 
 export default nextConfig;
