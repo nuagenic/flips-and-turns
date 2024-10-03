a02f39d3e691d797371f595b9d652a91f9eb51de
Baek Chang In <128976154+nuagenic@users.noreply.github.com>
2024-10-04 00:22:51 +0900

    Feat/add caption (#6)
    * feat: initialize Caption component
    
    * feat: Complete Caption Component
    
    * fix: Fix flexbox to fit Caption component below cards

---
+++ src/components/Caption/index.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { CardType } from "@/app/page";
+
+type Props = {
+  cards: CardType[];
+  currentIndex: number;
+};
+
+export default function Caption({ cards, currentIndex }: Props) {
+  const currentCard = cards[currentIndex];
+  const date = currentCard.createdAt;
+  function parseDate(num: number) {
+    return num < 10 ? "0" + num.toString() : num.toString();
+  }
+  const formattedDate =
+    date.getFullYear().toString().slice(2) +
+    "." +
+    parseDate(date.getMonth() + 1) +
+    "." +
+    parseDate(date.getDate());
+
+  const caption =
+    currentCard.createdBy +
+    "  |  " +
+    formattedDate +
+    "  |  " +
+    (currentIndex + 1) +
+    " / " +
+    cards.length;
+
+  return (
+    <div className="flex justify-center mt-2 mb-10 text-sm font-medium whitespace-pre">
+      {caption}
+    </div>
+  );
+}
