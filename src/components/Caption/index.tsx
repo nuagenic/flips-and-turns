04216eb224a81ac2b104ea8a1f1d3fdc9342747a
nuagenic <nuagenic@gmail.com>
2024-10-03 15:48:30 +0900

    feat: initialize Caption component

---
+++ src/components/Caption/index.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import { CardType } from "@/app/page";
+
+type Props = {
+  card: CardType;
+  currentIndex: number;
+};
+
+export default function Caption({ card, currentIndex }: Props) {
+  return;
+}
