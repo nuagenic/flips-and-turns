de87c748503171d15c98da1b6058daa8b5962636
Baek Chang In <128976154+nuagenic@users.noreply.github.com>
2025-01-02 17:20:28 +0900

    fix: Put rules and terms into white square (#27)

--- src/app/rules-and-terms/page.tsx
+++ src/app/rules-and-terms/page.tsx
@@ -3,28 +3,33 @@ import Header from "@/components/Header";
 
 export default function RulesAndTerms() {
   return (
-    <main className="z-0 flex h-full w-full items-start justify-center overflow-auto bg-basic px-4 py-32 lg:px-52">
+    <main className="z-0 flex h-full w-full items-center justify-center overflow-auto bg-basic px-4 py-32 lg:px-52">
       <Header />
-      <div className="z-20 flex flex-col items-start justify-center gap-10">
-        {rulesAndTerms.map((section) => {
-          return (
-            <section key={section.title}>
-              <div className="mb-2 font-semibold">{section.title}</div>
-              <ol>
-                {section.rules.map((rule, idx) => {
-                  return (
-                    <li className="font-normal leading-7" key={rule.content}>
-                      <span className="inline-block w-6">
-                        {idx + 1}.{"  "}
-                      </span>
-                      {rule.content}
-                    </li>
-                  );
-                })}
-              </ol>
-            </section>
-          );
-        })}
+      <div className="absolute z-20 aspect-square w-full transform overflow-auto bg-white p-12 text-xs text-black md:text-base lg:h-3/4vh lg:w-3/4vh">
+        <div className="z-20 flex flex-col items-start justify-center gap-10 text-xl">
+          {rulesAndTerms.map((section) => {
+            return (
+              <section key={section.title}>
+                <div className="ml-6 underline">{section.title}</div>
+                <ol>
+                  {section.rules.map((rule, idx) => {
+                    return (
+                      <li
+                        className="flex font-normal leading-7"
+                        key={rule.content}
+                      >
+                        <div className="w-6 flex-shrink-0">{idx + 1}.</div>
+                        <div className="w-auto whitespace-normal break-all">
+                          {rule.content}
+                        </div>
+                      </li>
+                    );
+                  })}
+                </ol>
+              </section>
+            );
+          })}
+        </div>
       </div>
     </main>
   );
