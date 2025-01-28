26d2a92c2d50a0579d7754772edfbfa44aa069b9
Baek Chang In <128976154+nuagenic@users.noreply.github.com>
2025-01-29 01:27:18 +0900

    fix: Change subscribe UI (#28)

--- src/app/subscribe/page.tsx
+++ src/app/subscribe/page.tsx
@@ -39,17 +39,18 @@ export default function Subscribe() {
   }
 
   return (
-    <main className="flex h-full w-full flex-grow flex-col items-center justify-center overflow-hidden bg-basic p-2">
+    <main className="z-0 flex h-full w-full items-center justify-center overflow-auto bg-basic px-4 py-32 lg:px-52">
       <Header />
-      <div className="flex grow flex-col justify-center">
-        <div className="z-20 mb-20 text-center text-2xl md:text-3xl">
-          we might be on the same page
+      <div className="z-20 flex aspect-square w-full transform flex-col justify-between overflow-auto bg-white p-12 text-xs text-black md:h-3/4vh md:w-3/4vh md:text-base lg:h-3/4vh lg:w-3/4vh">
+        <div className="z-20 flex w-full flex-col items-start justify-center gap-10 text-lg">
+          <Form updateItemAction={createSubscription} />
         </div>
-        <Form updateItemAction={createSubscription} />
-      </div>
-      <div className="z-20 mb-5 text-center text-xs">
-        200은 해치지 않습니다. 그래도 구독 해지를 원하시면{" "}
-        <b>flipsandturnsby200@gmail.com</b>으로 연락주세요.
+        <div className="z-20 mb-5 text-lg">
+          구독을 하시면 매주 업데이트 되는 flips and turns의 <br />
+          새로운 단위 작업을 이메일로 확인할 수 있습니다. <br />
+          200은 해치지 않습니다. 그래도 구독 해지를 원하시면
+          <b>flipsandturnsby200@gmail.com</b>으로 연락주세요.
+        </div>
       </div>
     </main>
   );
