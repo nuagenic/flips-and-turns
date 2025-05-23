8c2f8a011115204cb4a518a2bfd5da917529605d
nuagenic <nuagenic@gmail.com>
2025-05-23 15:41:35 +0900

    hotfix: font-size fix for rules-and-terms

--- src/app/rules-and-terms/page.tsx
+++ src/app/rules-and-terms/page.tsx
@@ -6,7 +6,7 @@ export default function RulesAndTerms() {
     <main className="z-0 flex h-full w-full items-center justify-center overflow-auto bg-basic px-2">
       <Header />
       <div className="z-20 aspect-square w-full transform overflow-auto bg-white p-12 text-black md:h-3/4vh md:w-3/4vh md:text-base lg:h-3/4vh lg:w-3/4vh">
-        <div className="z-20 flex flex-col items-start justify-center gap-10 text-sm md:text-lg">
+        <div className="z-20 flex flex-col items-start justify-center gap-10 text-xs md:text-sm">
           {rulesAndTerms.map((section) => {
             return (
               <section key={section.title}>
@@ -15,7 +15,7 @@ export default function RulesAndTerms() {
                   {section.rules.map((rule, idx) => {
                     return (
                       <li
-                        className="flex font-normal leading-5 md:leading-7"
+                        className="flex font-normal leading-5 md:leading-6"
                         key={rule.content}
                       >
                         <div className="w-6 flex-shrink-0">{idx + 1}.</div>
