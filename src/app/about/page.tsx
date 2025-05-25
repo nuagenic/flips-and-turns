b568452abf3558fdacd659158b449cb7fae23826
nuagenic <nuagenic@gmail.com>
2025-05-25 14:20:53 +0900

    fix: Apply typography throughout the pages

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -6,13 +6,13 @@ export default function About() {
   return (
     <main className="z-0 flex w-full flex-grow flex-col items-center justify-center overflow-auto bg-basic p-2">
       <Header />
-      <div className="z-20 flex aspect-square w-full transform flex-col justify-between overflow-auto bg-white p-10 text-black md:h-3/4vh md:w-3/4vh md:text-base lg:h-3/4vh lg:w-3/4vh">
-        <div className="z-20 flex flex-col items-start justify-center text-sm md:text-lg">
+      <div className="z-20 flex aspect-square w-full transform flex-col justify-between overflow-auto bg-white p-8 text-black md:h-3/4vh md:w-3/4vh md:p-12 md:text-base lg:h-3/4vh lg:w-3/4vh">
+        <div className="z-20 flex flex-col items-start justify-center break-keep text-xs leading-5 md:text-sm md:leading-6">
           <div>
             <span>flipsandturns는 200({"  "}</span>
             <FlippableLink char="이" href="https://www.leo-je-eon-lee.com/" />
             <span>{"   "}</span>
-            <FlippableLink char="백" href="https://brunch.co.kr/@bbaekcloud" />
+            <FlippableLink char="백" href="https://baekchangin.com" />
             <span>{"  "})이 작업한다</span>
           </div>
           <div>
@@ -21,7 +21,7 @@ export default function About() {
           <div>flipsandturns는 제주도에서 시작되었다</div>
           <div>
             <span> flipsandturns는{"  "}</span>
-            <FlippableLink char="백" href="https://brunch.co.kr/@bbaekcloud" />
+            <FlippableLink char="백" href="https://baekchangin.com" />
             <span>{"  "}의 졸업논문 주제다</span>
           </div>
           <div>flipsandturns는 어떻게 끝날지 아무도 모른다</div>
@@ -29,7 +29,7 @@ export default function About() {
             <span>flipsandturns는{"  "}</span>
             <FlippableLink char="이" href="https://www.leo-je-eon-lee.com/" />
             <span>{"  와   "}</span>
-            <FlippableLink char="백" href="https://brunch.co.kr/@bbaekcloud" />
+            <FlippableLink char="백" href="https://baekchangin.com" />
             <span> 의 대화와 크게 다르지 않다</span>
           </div>
           <div>flipsandturns는 주변 지인에게 큰 호응을 얻지 못했다</div>
--- src/app/subscribe/page.tsx
+++ src/app/subscribe/page.tsx
@@ -41,15 +41,21 @@ export default function Subscribe() {
   return (
     <main className="z-0 flex h-full w-full items-center justify-center overflow-auto bg-basic px-2">
       <Header />
-      <div className="z-20 flex aspect-square w-full transform flex-col justify-between overflow-auto bg-white p-10 text-black md:h-3/4vh md:w-3/4vh md:text-base lg:h-3/4vh lg:w-3/4vh">
-        <div className="z-20 flex w-full flex-col items-start justify-center gap-10 text-sm md:text-lg">
+      <div className="z-20 flex aspect-square w-full transform flex-col justify-between overflow-auto bg-white p-8 text-black md:h-3/4vh md:w-3/4vh md:p-12 md:text-base lg:h-3/4vh lg:w-3/4vh">
+        <div className="z-20 flex w-full flex-col items-start justify-center gap-10 text-xs md:text-sm">
           <Form updateItemAction={createSubscription} />
         </div>
-        <div className="z-20 text-sm md:text-lg">
+        <div className="z-20 break-keep text-xs leading-5 md:text-sm md:leading-6">
           구독을 하시면 매주 업데이트 되는 flips and turns의 <br />
           새로운 단위 작업을 이메일로 확인할 수 있습니다. <br />
           200은 해치지 않습니다. 그래도 구독 해지를 원하시면
-          <b>flipsandturnsby200@gmail.com</b>으로 연락주세요.
+          <b>
+            {" "}
+            <a href="mailto:flipsandturnsby200@gmail.com">
+              flipsandturnsby200@gmail.com
+            </a>
+          </b>
+          으로 연락주세요.
         </div>
       </div>
     </main>
