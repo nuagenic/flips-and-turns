108924a8a7e427f5656b14c82fd9207b815e4b43
Baek Chang In <128976154+nuagenic@users.noreply.github.com>
2025-05-25 14:28:54 +0900

    Fix/font size (#36)
    * fix: Apply typography throughout the pages
    
    * fix: Modify introduction text
    
    * fix: Modify transition time

--- src/app/rules-and-terms/page.tsx
+++ src/app/rules-and-terms/page.tsx
@@ -5,7 +5,7 @@ export default function RulesAndTerms() {
   return (
     <main className="z-0 flex h-full w-full items-center justify-center overflow-auto bg-basic px-2">
       <Header />
-      <div className="z-20 aspect-square w-full transform overflow-auto bg-white p-12 text-black md:h-3/4vh md:w-3/4vh md:text-base lg:h-3/4vh lg:w-3/4vh">
+      <div className="z-20 aspect-square w-full transform overflow-auto bg-white p-8 text-black md:h-3/4vh md:w-3/4vh md:p-12 lg:h-3/4vh lg:w-3/4vh">
         <div className="z-20 flex flex-col items-start justify-center gap-10 break-keep text-xs md:text-sm">
           {rulesAndTerms.map((section) => {
             return (
